b5e79dfa605aaf2e6f68353495f75065193994a6
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-03-21 17:59:17 -0400

    created and solved grok-ex8.py - grok-ex12.py

--- grok-ex7.py
+++ grok-ex7.py
@@ -1,3 +1,9 @@
 # Question 7 (String Basics)
-#You have this string: phrase = "Hi There". Write a line of code (no loop) to:
+#You have this string: phrase = "Hi there". Write a line of code (no loop) to:
 # Print the string with the first letter of each word capitalized (title case).
+
+phrase = "Hi there"
+
+title_phrase = phrase.title()
+
+print(title_phrase)
