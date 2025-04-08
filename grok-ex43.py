79058adb5a48edd5682ec3c9b935308875fd913d
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-04-08 19:38:36 -0400

    created and solved grok-ex43.py

---
+++ grok-ex43.py
@@ -0,0 +1,12 @@
+# colors = ["red", "blue", "green"]
+
+# Print each color with "!" at the end.
+
+# Expected output:
+# red!
+# blue!
+# green!
+
+colors = ["red", "blue", "green"]
+for i in range(len(colors)):
+    print(colors[i] + "!")
