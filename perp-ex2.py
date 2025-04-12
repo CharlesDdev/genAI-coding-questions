e607834b466f6816b3c92a2ade77997bc8ba0801
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-04-12 16:17:31 -0400

    Update perp-ex2.py

--- perp-ex2.py
+++ perp-ex2.py
@@ -1,5 +1,11 @@
-# Write a function called cumulative_sum that takes a list of numbers as input and returns a new list where each element at index i is the sum of all input list elements from index 0 up to i.
-For example:
-	•	 Given the list, your function should return.
-	•	 Given the list , the function should return.
-	•	 An empty list should return an empty list.
+# Write a function called cumulative_sum
+# that takes a list of numbers as input
+# and returns a new list where each element at index i is the sum of all input list elements from index 0 up to i.
+
+def cumulative_sum(numslist):
+    cumulative = []
+    total = 0
+    for num in numslist:
+        total += num
+        cumulative.append(total)
+    return cumulative
