120da5111dc7146b0498f58b6d3eb16d57ade977
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-04-12 16:33:54 -0400

    updated perp-ex3.py but not completed

--- perp-ex3.py
+++ perp-ex3.py
@@ -4,3 +4,9 @@
 # For example:
 	#	is_palindrome(“A man, a plan, a canal, Panama”) should return True.
 	#	is_palindrome(“python”) should return False.
+
+def is_palindrome(text):
+    normalized = join(char.lower()
+    for char in text:
+        if char isalnum():
+            return normalized
