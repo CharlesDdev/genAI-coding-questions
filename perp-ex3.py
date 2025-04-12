2558653ad8ae708d6c42e9d12c014a764886abc1
CharlesD <185566712+CharlesDdev@users.noreply.github.com>
2025-04-12 16:19:13 -0400

    Create perp-ex3.py

---
+++ perp-ex3.py
@@ -0,0 +1,6 @@
+# Write a function called is_palindrome that takes a string as input
+# and returns True if the string is a palindrome and False otherwise.
+# When checking for a palindrome, ignore spaces, punctuation, and letter casing.
+# For example:
+	#	is_palindrome(“A man, a plan, a canal, Panama”) should return True.
+	#	is_palindrome(“python”) should return False.
